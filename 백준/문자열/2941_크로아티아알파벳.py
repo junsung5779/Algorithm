0eb9bd56f31461af42b437c17d801bd9fe54a059
juns5779 <juns5779@naver.com>
2021-12-30 02:03:04 +0900

    백준 크로아티아 알파벳

--- 백준/문자열/2941_크로아티아알파벳.py
+++ 백준/문자열/2941_크로아티아알파벳.py
@@ -1,5 +1,8 @@
-
-def croatia():
-    arr1 = ['c=','c-','dz=','d-','lj','nj','s=','z=']
-    return 1
-print(croatia())
+word = input()
+arr1 = ['c=','c-','dz=','d-','lj','nj','s=','z=']
+# 리스트를 반복하면서
+for i in arr1:
+    # 크로아티아 알파벳만 문자열"*"로 대체시킨다
+    word = word.replace(i, '*')
+# word의 길이를 반환
+print(len(word))
