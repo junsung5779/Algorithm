33e188a68b49b57ada934ace48190d3e06549850
hamoon <junsung5779@gmail.com>
2022-09-25 01:15:58 +0900

    백준_1874_스택수열

--- 알고리즘 스터디/큐&스택/백준_18258_큐2.py
+++ 알고리즘 스터디/큐&스택/백준_18258_큐2.py
@@ -1,51 +1,43 @@
 # ########## hamoon의 풀이 ##########
 import sys
+from collections import deque
 sys.stdin = open('input18528.txt')
-# 테스트케이스 반복횟수를 입력받는다.
-T = int(input())
-# 정수를 저장하는 큐 생성 및 초기화
-queue = []
-#테스트케이스를 반복하면서
-for i in range(T):
-    # 단어를 입력받아
-    word = str(input())
-    # 케이스에 따라 분류한다.
-
-    # push인 경우 (해당 문자열 길이가 push(공백)(숫자) 이기 때문에 최소 길이가 6이다)
-    if len(word) >= 6:
-        # 정수 X를 큐에 넣는다.
-            # 해당 문자열의 마지막 단어(숫자)를 큐에 넣는다.
-        queue.append(word[-(len(word)-5)])
+T = int(sys.stdin.readline())   # 테스트케이스 횟수 입력받기
+queue = deque([])
+for tc in range(T): # 테스트케이스 횟수만큼 반복하면서
+    stack = sys.stdin.readline().split() # 띄워쓰기를 구분점으로 입력받는다.
+    # push인 경우
+    if stack[0] == 'push':
+        queue.append(stack[1])  # 숫자를 append
     # pop인 경우
-    elif word == 'pop':
-        if queue:  # 큐에 정수가 들어있는 경우
-            print(queue.pop(0)) #큐에서 가장 앞에 있는 정수를 빼고, 그 수를 출력
-            # 리스트로 선언해서 pop(0)를 하게 되면, 첫 번째 요소를 pop 하고나서 나머지 요소들의 인덱스를 1칸씩 당기는 과정에서 O(n)의 계산량이 발생하기 때문에 시관초과
-        else:  # 큐에 들어가있는 정수가 없는 경우 -1을 출력
+    elif stack[0] == 'pop':
+        if queue:
+            print(queue.popleft())
+        else:
             print(-1)
     # size인 경우
-    elif word == 'size':
-        print(len(queue))   # 큐에 들어있는 정수의 개수를 출력
+    elif stack[0] == 'size':
+        print(len(queue))
     # empty인 경우
-    elif word == 'empty':
-        # 큐가 비어있으면 1, 아니면 0을 출력
+    elif stack[0] == 'empty':
         if queue:
             print(0)
         else:
             print(1)
     # front인 경우
-    elif word == 'front':
-        if queue:  # 큐에 정수가 들어있는 경우
-            print(queue[0])     #큐의 가장 앞에 있는 정수를 출력
-        else:  # 큐에 들어가있는 정수가 없는 경우 -1을 출력
+    elif stack[0] == 'front':
+        if queue:
+            print(queue[0])
+        else:
             print(-1)
     # back인 경우
-    elif word == 'back':
-        if queue:  # 큐에 정수가 들어있는 경우
-            print(queue[-1])    #큐의 가장 뒤에 있는 정수를 출력
-        else:  # 큐에 들어가있는 정수가 없는 경우 -1을 출력
+    elif stack[0] == 'back':
+        if queue:
+            print(queue[-1])
+        else:
             print(-1)
-#
+        
+
 # # ['push 1', 'push 2', 'front', 'back', 'size', 'empty', 'pop', 'pop', 'pop', 'size', 'empty', 'pop', 'push 3', 'empty', 'front']
 # # print(list)
 
