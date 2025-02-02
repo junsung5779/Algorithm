7bbd8533662fd6874d75a12fbfc54a0e46cbb493
Choi Junsung <77382646+junsung5779@users.noreply.github.com>
2025-02-03 01:07:37 +0900

    [level 1] Title: 모의고사, Time: 11.70 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=javascript) 
 
 ### 성능 요약
 
-메모리: 38.5 MB, 시간: 10.41 ms
+메모리: 38.7 MB, 시간: 11.70 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 03일 00:55:05
+2025년 02월 03일 01:07:36
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,4 +1,6 @@
+
 function solution(answers) {
+    // 해설을 보고 푼 문제입니다.
     // 수포자들의 정답 패턴
     const patterns = [
         [1,2,3,4,5],
@@ -8,9 +10,16 @@ function solution(answers) {
     // 수포자들의 점수를 저장할 배열
     const scores = [0,0,0]
     
-    // 각 수포자들의 패턴과 정답이 얼마나 일치하는지 확인
+    /**
+    * 각 수포자들의 패턴과 정답이 얼마나 일치하는지 확인
+    * entries() 는 배열 내 데이터를 인덱스와 값으로 묶어서 순회할 수 있는 Iterator 객체를 반환함.
+    * Iterator 객체를 of 연산자를 이용하면 처음부터 끝까지 순회하는 것이 가능함.
+    * 정답 패턴의 길이가 수포자의 답안 길이보다 긴 경우 정답 패턴의 처음 데이터와 다시 비교할 수 있도록 나머지 연산자 사용.
+    */ 
+    
     for (const [i, answer] of answers.entries()) {
         for (const [j, pattern] of patterns.entries()) {
+            
             if (answer === pattern[i % pattern.length]) {
                 scores[j] += 1;
             }
