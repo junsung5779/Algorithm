d2015490aeff727450714a42c58505537955e2e2
Choi Junsung <77382646+junsung5779@users.noreply.github.com>
2025-09-25 21:40:06 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -0,0 +1,9 @@
+function solution(array, height) {
+    let answer = 0;
+    for(const person of array) {
+        if(person > height) {
+            answer += 1;
+        }
+    }
+    return answer;
+}
