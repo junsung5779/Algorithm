6a073915ead136f3292ea9d06116019ba0236740
Choi Junsung <77382646+junsung5779@users.noreply.github.com>
2025-09-26 22:46:51 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.js
@@ -0,0 +1,10 @@
+function solution(dot) {
+    let answer = 0;
+    const [x, y] = dot;
+    if (x > 0) {
+        return y > 0 ? 1 : 4;
+    } else {
+        return y > 0 ? 2 : 3;
+    }
+    return answer;
+}
