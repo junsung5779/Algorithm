0b44df46086d858991a2e4c388fd31ce24cd44e2
Choi Junsung <77382646+junsung5779@users.noreply.github.com>
2025-10-04 18:26:27 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 4.64 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -0,0 +1,11 @@
+function solution(num_list) {
+    let answer = -1;
+    for(let i=0; i<num_list.length; i++) {
+        console.log(i)
+        if(num_list[i] < 0) {
+            answer = i;
+            break;
+        }
+    }
+    return answer;
+}
