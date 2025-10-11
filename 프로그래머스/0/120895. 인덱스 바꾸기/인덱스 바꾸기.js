393f035a383767d196322d6424c7e0770bf585f9
Choi Junsung <77382646+junsung5779@users.noreply.github.com>
2025-10-11 15:46:39 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,14 @@
+function solution(my_string, num1, num2) {
+    const answer = [];
+    const tmp = [];
+    for(let i=0; i<my_string.length; i++) {
+        if (i===num1) {
+            answer.push(my_string[num2]);
+        } else if (i===num2) {
+            answer.push(my_string[num1]);
+        } else {
+            answer.push(my_string[i])
+        }
+    }
+    return answer.join('');
+}
