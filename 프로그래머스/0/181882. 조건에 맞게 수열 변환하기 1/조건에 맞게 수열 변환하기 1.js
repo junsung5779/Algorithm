f8b886f6611d17c275a73d0149223b230ce852c4
Choi Junsung <77382646+junsung5779@users.noreply.github.com>
2025-10-17 22:20:48 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 41.35 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+    return arr.map((a)=>{
+        if(a>=50 && a%2===0) {
+            return (a/2);
+        }
+        if(a<50 && a%2===1) {
+            return (a*2);
+        }
+        return a;
+    });
+}
